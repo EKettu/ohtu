6ac5ccc7e365cf362dce741d68de64989ac24cb8
EKettu <elina@Elinas-MacBook-Pro.local>
2017-04-06 17:50:40 +0300

    eka muutos

--- viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String studentNr = "01234567";
+        String studentNr = "012933000";
         if (args.length > 0) {
             studentNr = args[0];
         }
