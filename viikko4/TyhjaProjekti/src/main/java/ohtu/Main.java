0292de73468050fc58a3b5c75fbc4445f42cfc91
EKettu <elina@Elinas-MacBook-Pro.local>
2017-04-06 18:24:18 +0300

    viikon 4 tehtäviä

--- viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String studentNr = "012933000";
+        String studentNr = "012345678";
         if (args.length > 0) {
             studentNr = args[0];
         }
